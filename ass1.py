2c58ba2821d816ea4cd21508546bc901bf0e51b9
mattsklivas <msklivas@live.com>
2021-01-26 18:28:54 -0500

    add files

---
+++ ass1.py
@@ -0,0 +1,13 @@
+import threading
+
+def mergesort(arr: list):
+    return None
+
+if __name__ == '__main__':
+    file = open('input.txt', mode = 'r', encoding = 'utf-8-sig')
+    lines = file.readlines()
+    file.close()
+    values = list()
+    for i in range(len(lines)):
+        values.append(int(lines[i].strip('\n')))
+    mergesort(values)
