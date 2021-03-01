b9fed1fadcbc61f7983b8515fff0dfe1068ce3c7
mattsklivas <msklivas@live.com>
2021-03-01 16:34:18 -0500

    add process class

--- ass2/assignment2.py
+++ ass2/assignment2.py
@@ -10,38 +10,53 @@
 import threading
 from concurrent.futures import ThreadPoolExecutor
 
+# global dict used to get the name of each event state corresponding to a certain integer
+event_dict = {0: "Started", 1: "Resumed", 2: "Paused", 3: "Finished"}
+
 # class used to handle the fair-share process scheduling and write to output.txt
 class Scheduler:
 
     # initialize the output file for writing
     def __init__(self, quant: int):
-        self.quantum = quant
         self.output = open("output.txt", "w")
 
+class Process:
 
+    def __init__(self, user: str, quant: int, ready_t: int, service_t: int):
+        self.user_id = user
+        # amount of quantum time allocated to process
+        self.quantum = quant
+        # time at which the process is ready to be executed
+        self.ready_time = ready_t
+        # time left until process completes its execution
+        self.time_left = service_t
+        # type of event state -> 1: started, 2: resumed, 3: paused, 4: finished
+        self.state = None
+    
+    
 # entrypoint of script execution
 if __name__  == '__main__':
 
-    # open input.txt and read contents
+    # open input.txt and read file content
     with open('input.txt', 'r') as file:
         lines = file.readlines()
 
-    # get the time quantum
+    # get the time quantum (equally distributed across processes in program)
     quant = int(lines.pop(0))
 
-    # list containing the number of processes for each user
-    num_processes = []
+    # dictionary containing the user names and number of processes under each user
+    user_dict = {}
 
-    # list containing the ready time for each process
+    # list containing the ready time for each process (time at which the process is ready)
     ready_time = []
 
-    # list containing the service time for each process
+    # list containing the service time for each process (total CPU usage time needed for a process)
     service_time = []
 
     # get the characteristics of each process for each user
     for line in lines:
         if line[0].isalpha():
-            num_processes.append(int(line.split(" ")[1].rstrip()))
+            user_dict[line.split(" ")[0]] = int(line.split(" ")[1].rstrip())
         else:
             ready_time.append(int(line.split(" ")[0]))
             service_time.append(int(line.split(" ")[1].rstrip()))
@@ -49,6 +64,22 @@ if __name__  == '__main__':
     # close the file
     file.close()
 
-    print(num_processes)
-    print(ready_time)
-    print(service_time)
+    # list for the allocation of time quantums
+    quant_alloc = []
+
+    # find the amount of the time quantum allocated to each process
+    num_users = len(user_dict) # number of users
+    for key in user_dict:
+        num_user_processes = user_dict[key]
+        for i in range(0, num_user_processes):
+            quant_alloc.append(int(quant//num_users//num_user_processes))
+
+    # array of Process objects to be executed
+    processes = []
+
+    # generate a Process object and add it to the list of processes
+    counter = 0  # counter used to traverse each previously generated list for process specifications
+    for user in user_dict:
+        for i in range(0, user_dict[user]):
+            processes.append(Process(user,quant_alloc[counter],ready_time[counter],service_time[counter]))
+            counter += 1
